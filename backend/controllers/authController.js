5e69e37cf35550352bfaf415ee9aaa85e23ea66e
Brooke Hasty <brookehasty777@gmail.com>
2025-12-27 09:59:31 -0500

    authController register created

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -13,7 +13,45 @@ const generateToken = (id) => {
 // @access  Public
 export const register = async (req, res, next) => {
     try {
+        const { username, email, passward } = req.body;
 
+        // Check if user exists
+        const userExists = await User.findOne({ $or: [{ email }] });
+
+        if (userExists) {
+            return res.status(400).json({
+                success: false,
+                error:
+                    userExists.email === email
+                        ? 'Email already registered'
+                        : 'Username already taken',
+                    statusCode: 400
+            });
+        }
+        // Create user
+        const user = await User.create({
+            username,
+            email,
+            password,
+        });
+
+        // Generate token
+        const token = generateToken(user._id);
+
+        res.status(201).json({
+            success: true,
+            data: {
+                user: {
+                    id: user._id,
+                    username: user.username,
+                    email: user.email,
+                    profileImage: user.profileImage,
+                    createdAt: user.createdAt,
+                },
+                token,
+            },
+            message: 'User registered successfully',
+        });
     } catch (error) {
         next(error);
     }
@@ -22,25 +60,41 @@ export const register = async (req, res, next) => {
 // @desc    Login user
 // @route   POST /api/auth/login
 export const login = async (req, res, next) => {
+    try {
 
+    } catch (error) {
+        next(error);
+    }
 };
 
 // @desc    Get user profile
 // @route   GET /api/auth/profile
 // @access  Private
 export const getProfile = async (req, res, next) => {
+    try {
 
+    } catch (error) {
+        next(error);
+    }
 };
 
 // @desc    Update user profile
 // @route   PUT /api/auth/profile
 // @access  Private
 export const updateProfile = async (req, res, next) => {
+    try {
 
+    } catch (error) {
+        next(error);
+    }
 };
 // @desc    Change password
 // @route   POST /api/auth/change-password
 // @access  Private
 export const changePassword = async (req, res, next) => {
+    try {
 
+    } catch (error) {
+        next(error);
+    }
 };
